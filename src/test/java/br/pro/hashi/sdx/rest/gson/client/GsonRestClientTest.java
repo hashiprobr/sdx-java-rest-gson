5fd365c49465aa2849ebac0d82d5582f067bbb59
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-13 15:50:05 -0300

    Complete basic tests

--- src/test/java/br/pro/hashi/sdx/rest/gson/client/GsonRestClientTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/gson/client/GsonRestClientTest.java
@@ -1,4 +1,35 @@
 package br.pro.hashi.sdx.rest.gson.client;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import br.pro.hashi.sdx.rest.client.RestClient;
+
 class GsonRestClientTest {
+	private RestClient client;
+
+	@BeforeEach
+	void setUp() {
+		client = mock(RestClient.class);
+	}
+
+	@Test
+	void builds() {
+		try (MockedConstruction<GsonRestClientBuilder> construction = mockBuilderConstruction()) {
+			assertSame(client, GsonRestClient.to("http://a"));
+		}
+	}
+
+	private MockedConstruction<GsonRestClientBuilder> mockBuilderConstruction() {
+		return mockConstruction(GsonRestClientBuilder.class, (mock, context) -> {
+			when(mock.build(any())).thenReturn(client);
+		});
+	}
 }
--- src/test/java/br/pro/hashi/sdx/rest/gson/server/GsonRestServerTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/gson/server/GsonRestServerTest.java
@@ -1,4 +1,35 @@
 package br.pro.hashi.sdx.rest.gson.server;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import br.pro.hashi.sdx.rest.server.RestServer;
+
 class GsonRestServerTest {
+	private RestServer server;
+
+	@BeforeEach
+	void setUp() {
+		server = mock(RestServer.class);
+	}
+
+	@Test
+	void builds() {
+		try (MockedConstruction<GsonRestServerBuilder> construction = mockBuilderConstruction()) {
+			assertSame(server, GsonRestServer.from("package"));
+		}
+	}
+
+	private MockedConstruction<GsonRestServerBuilder> mockBuilderConstruction() {
+		return mockConstruction(GsonRestServerBuilder.class, (mock, context) -> {
+			when(mock.build(any())).thenReturn(server);
+		});
+	}
 }
