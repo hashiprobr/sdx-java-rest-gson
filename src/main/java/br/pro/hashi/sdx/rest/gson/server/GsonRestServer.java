433f2e29c42a23b97132ae188dff4ea74f517941
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-18 14:35:57 -0300

    Add convenience build methods

--- src/main/java/br/pro/hashi/sdx/rest/gson/server/GsonRestServer.java
+++ src/main/java/br/pro/hashi/sdx/rest/gson/server/GsonRestServer.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.sdx.rest.gson.server;
 
 import br.pro.hashi.sdx.rest.server.RestServer;
+import br.pro.hashi.sdx.rest.server.RestServerBuilder;
 
 /**
  * Convenience class to quickly build a REST server with Gson support.
@@ -10,10 +11,19 @@ public final class GsonRestServer {
 	 * Instantiates a REST server using the resources of a specified package.
 	 * 
 	 * @param packageName the package name
-	 * @return the REST server
+	 * @return the server
 	 */
 	public static RestServer from(String packageName) {
-		return new GsonRestServerBuilder().build(packageName);
+		return builder().build(packageName);
+	}
+
+	/**
+	 * Convenience method for instantiating a REST server builder.
+	 * 
+	 * @return the server builder
+	 */
+	public static RestServerBuilder builder() {
+		return new GsonRestServerBuilder();
 	}
 
 	private GsonRestServer() {
