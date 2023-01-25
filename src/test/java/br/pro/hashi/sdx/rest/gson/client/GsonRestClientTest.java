457681fe9a9d03242ef2db62b43c3a07ec7daa4e
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-25 14:03:23 -0300

    Align with upstream

--- src/test/java/br/pro/hashi/sdx/rest/gson/client/GsonRestClientTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/gson/client/GsonRestClientTest.java
@@ -4,30 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
 import br.pro.hashi.sdx.rest.client.RestClient;
+import br.pro.hashi.sdx.rest.client.RestClientBuilder;
 
 class GsonRestClientTest {
 	private RestClient client;
-
-	@BeforeEach
-	void setUp() {
-		client = mock(RestClient.class);
-	}
 
 	@Test
 	void builds() {
 		try (MockedConstruction<GsonRestClientBuilder> construction = mockBuilderConstruction()) {
 			assertSame(client, GsonRestClient.to("http://a"));
+			RestClientBuilder builder = construction.constructed().get(0);
+			verify(builder).build("http://a");
 		}
 	}
 
 	private MockedConstruction<GsonRestClientBuilder> mockBuilderConstruction() {
+		client = mock(RestClient.class);
 		return mockConstruction(GsonRestClientBuilder.class, (mock, context) -> {
 			when(mock.build(any())).thenReturn(client);
 		});
